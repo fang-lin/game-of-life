0ff5bb2795f42c7db1e0d820e47a19d56a466385
Lin Fang <linfang@Lins-old-MacBook-Pro.local>
2023-01-11 12:12:24 +0100

    update wiki link

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -3,7 +3,7 @@ import {FooterWrapper, Anchor} from './Footer.styles';
 
 function Footer() {
     return <FooterWrapper>
-        <Anchor href="https://conwaylife.com/wiki/" target="_blank">conwaylife.com wiki</Anchor> | <Anchor
+        <Anchor href="https://conwaylife.com/wiki/" target="_blank">LifeWiki</Anchor> | <Anchor
             href="https://github.com/fang-lin/game-of-life" target="_blank">GitHub</Anchor> | <Anchor
             href="https://www.fanglin.me/" target="_blank">Lin Fang in {(new Date()).getFullYear()}</Anchor> | <Anchor
             href="https://plotter.fanglin.me/" target="_blank">Function Plotter</Anchor> | <Anchor
