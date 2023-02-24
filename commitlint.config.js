7683f7bb5851282b1baeb41de67de3c756bc32b1
Lin Fang <fanglin.me@gmail.com>
2023-02-24 03:21:16 +0100

    fix: use commit preset

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,4 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
-    rules: {
-        'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert']]
-    }
+    parserPreset: 'conventional-changelog-conventionalcommits',
 };
